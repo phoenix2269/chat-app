b77f8ee229df6d449a8e2ec99cc1254833a88e39
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-10-07 18:32:22 -0400

    Added code for AsyncStorage and online-offline

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, query, orderBy } from 'firebase/firestore';
 
-const Chat = ({ db, route, navigation }) => {
+const Chat = ({ db, route, navigation, isConnected }) => {
     const [messages, setMessages] = useState([]);
     const { name, color, userID } = route.params;
 
@@ -12,24 +13,44 @@ const Chat = ({ db, route, navigation }) => {
     useEffect(() => {
         navigation.setOptions({ title: name });
  
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-        const unsubMessages = onSnapshot(q, (docs) => {
-            let newMessages = [];
-            docs.forEach(doc => {
-                newMessages.push({
-                    id: doc.id,
-                    ...doc.data(),
-                    createdAt: new Date(doc.data().createdAt.toMillis())
-                })
+        if (isConnected === true) {
+            // unregister current onSnapshot() listener to avoid registering multiple listeners when
+            // useEffect code is re-executed.
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+            unsubMessages = onSnapshot(q, (docs) => {
+                let newMessages = [];
+                docs.forEach(doc => {
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis())
+                    })
+                });
+                setMessages(newMessages);
             });
-            setMessages(newMessages);
-        });
+        } else loadCachedMessages();
 
         // Clean up code
         return () => {
             if (unsubMessages) unsubMessages();
         }
-    }, []);
+    }, [isConnected]);
+
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const loadCachedMessages = async () => {
+        const cachedMessages = await AsyncStorage.getItem("messages") || [];
+        setMessages(JSON.parse(cachedMessages));
+    }
 
     const onSend = (newMessages) => {
         addDoc(collection(db, "messages"), newMessages[0])
@@ -40,13 +61,19 @@ const Chat = ({ db, route, navigation }) => {
             {...props}
             wrapperStyle={{
                 right: {
-                backgroundColor: "#000"
+                    backgroundColor: "#000"
                 },
                 left: {
-                backgroundColor: "#FFF"
+                    backgroundColor: "#FFF"
                 }
             }}
         />
+    }
+
+    // Render Custom toolbar
+    const renderInputToolbar = (props) => {
+        if (isConnected) return <InputToolbar {...props} />;
+        else return null;
     }
 
     return (
